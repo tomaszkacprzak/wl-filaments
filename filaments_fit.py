92812eebf98cdd3f1c7fc8528b5132143c8c5dd1
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2014-03-22 15:46:13 +0000

    changed back to agg

--- filaments_fit.py
+++ filaments_fit.py
@@ -1,5 +1,5 @@
 import matplotlib as mpl
-mpl.use('tkagg')
+mpl.use('agg')
 import os, yaml, argparse, sys, logging , pyfits, galsim, emcee, tabletools, cosmology, filaments_tools, plotstools, mathstools, scipy, scipy.stats
 import numpy as np
 import matplotlib.pyplot as pl
@@ -32,176 +32,6 @@ h = hpy()
 dtype_stats = {'names' : ['id','kappa0_signif', 'kappa0_map', 'kappa0_err_hi', 'kappa0_err_lo', 'radius_map',    'radius_err_hi', 'radius_err_lo', 'chi2_red_null', 'chi2_red_max',  'chi2_red_D', 'chi2_red_LRT' , 'chi2_null', 'chi2_max', 'chi2_D' , 'chi2_LRT' ,'sigma_g' ] , 
         'formats' : ['i8'] + ['f8']*16 }
 
-# def process_results():
-
-#     filename_prob = 'results.prob.shears_bcc_g.pp2'
-#     filename_prob_array = filename_prob.replace('.prob.', '.prob-array.')
-#     file_grid = open(filename_prob,'r')
-#     filename_grid = 'results.grid.bcc.pp2'
-#     file_prob = open(filename_prob,'r')
-#     n_pairs = 3000
-
-#     import cPickle as pickle
-
-#     grid=pickle.load(file_grid)
-#     n_grid = len(grid['kappa0_post'])
-
-#     prob_post_array = np.zeros([n_pairs,n_grid],dtype=np.float32)
-
-#     for n in range(n_pairs):
-#         res=pickle.load(file_prob)
-#         prob_post_array[n,:] = res['prob_post']
-#         print n
-
-#     print prob_post_array.shape
-
-#     tabletools.savePickle(filename_prob_array, prob_post_array)
-
-#     # prob_post_matrix = np.reshape(  prob_post , [n_grid, n_grid] )    
-#     # prob_post_kappa0 = prob_post_matrix.sum(axis=1)
-#     # prob_post_radius = prob_post_matrix.sum(axis=0)
-
-
-#     pl.imshow(prob_post_array)
-#     pl.show()
-
-def analyse_stats():
-
-    filename_results_stats = 'results.stats.shears_bcc_e.cat'
-    stats = tabletools.loadTable(filename_results_stats,dtype=dtype_stats)
-
-    # plotstools.plot_dist( np.array([ stats['kappa0_map'] , stats['radius_map'] ]) )
-    # pl.show()
-    pl.figure()
-    pl.subplot(121)
-    pl.hist(stats['kappa0_map'],bins=50)
-    pl.xlabel('kappa0 ML')
-    pl.subplot(122)
-    pl.hist(stats['radius_map'],bins=50)
-    pl.xlabel('radius ML')
-
-    filename_fig = 'figs/stats.histograms.png'
-    pl.savefig(filename_fig)
-    log.info('saved %s' , filename_fig)
-
-    print 'fraction of filaments with kappa > 0.0005'
-    select_k = (stats['kappa0_map'] > 0.0005) * (stats['kappa0_map'] < 0.025)
-    print sum(select_k) 
-
-    print 'fraction of filaments with  0.5 > radius < 8 '
-    select_r = (stats['radius_map'] < 8) * (stats['radius_map'] > 0.25)
-    print sum(select_r) 
-
-    print 'fraction of filaments with valid kappa and radius'
-    print sum( select_r * select_k )           
-
-    valid_stats = stats[select_r * select_k]
-
-    import pdb; pdb.set_trace()
-    kappa0_mean = np.mean(valid_stats['kappa0_map']) 
-    kappa0_err = ( valid_stats['kappa0_err_hi'] + valid_stats['kappa0_err_lo'] ) / 2. 
-    kappa0_stdm = np.sqrt( sum(kappa0_err**2) ) / len(kappa0_err)
-
-    print kappa0_mean , kappa0_stdm
-
-    pl.figure()
-    pl.scatter(stats['kappa0_map'][select_r],stats['radius_map'][select_r],s=20,c='r',marker='o')
-    pl.scatter(stats['kappa0_map'][select_k],stats['radius_map'][select_k],s=20,c='g',marker='o')
-    pl.scatter(stats['kappa0_map'][select_r * select_k],stats['radius_map'][select_r * select_k],s=20,c='b',marker='o')
-    pl.xlabel('kappa0')
-    pl.ylabel('radius')
-    filename_fig = 'figs/stats.scatter_radius_kappa0.png'
-    pl.savefig(filename_fig)
-    log.info('saved %s' , filename_fig)
-    # pl.show()
-
-    filename_results_grid= 'results.grid.pp2'
-    grid = tabletools.loadPickle(filename_results_grid)
-    import pdb; pdb.set_trace()
-
-
-    filename_results_pdfs= 'results.prob.shears_bcc_e.pp2'
-    pdfs = np.array(tabletools.loadPickle(filename_results_pdfs))
-    pdfs_sum = np.sum(pdfs[select_r * select_k],axis=0)
-
-    log_pdfs = np.log(pdfs)
-
-    pdfs_prod = np.sum(log_pdfs[select_r * select_k],axis=0)
-    pdfs_prod = np.exp(pdfs_prod - max(pdfs_prod)) 
-    pdfs_prod = pdfs_prod / sum(pdfs_prod)
-
-    n_grid = int(np.sqrt(len(pdfs[0])))
-    import pdb; pdb.set_trace()
-    
-    prob_post_matrix = np.reshape(  pdfs_prod , [n_grid, n_grid] )    
-    prob_post_kappa0 = prob_post_matrix.sum(axis=1)
-    prob_post_radius = prob_post_matrix.sum(axis=0)
-
-    # pl.imshow(prob_post_matrix)
-    # pl.show()
-
-    pl.figure()
-    plotstools.imshow_grid(grid['kappa0_post'],grid['radius_post'],pdfs_prod)
-    pl.show()
-
-
-    print pdfs.shape
-
-    import pdb; pdb.set_trace()
-
-
-    pass    
-
-# def analyse_results():
-
-#     filename_prob_array = 'results.prob.shears_bcc_g.pp2'
-#     res_prob_array = tabletools.loadPickle(filename_prob_array)[0]
-#     n_pairs = res_prob_array.shape[0]
-
-#     filename_grid = 'results.grid.bcc.pp2'
-#     file_grid = open(filename_grid,'r')
-#     import cPickle as pickle
-
-#     grid=pickle.load(file_grid)
-#     # ['grid_radius', 'grid_kappa0', 'radius_post', 'kappa0_post']
-#     n_grid = grid['grid_kappa0'].shape[0]
-    
-#     # prob_prod = np.prod(res_prob_array,axis=0)
-
-#     # prob_post_matrix = np.reshape(  prob_prod , [n_grid, n_grid] )    
-#     # prob_post_kappa0 = prob_post_matrix.sum(axis=1)
-#     # prob_post_radius = prob_post_matrix.sum(axis=0)
-
-#     # pl.imshow(prob_post_matrix)
-#     # pl.colorbar()
-#     # pl.show()
-
-#     filename_pairs = 'results.pairs.bcc.cat'
-#     pairs_results = tabletools.loadTable(filename_pairs,dtype=dtype_stats)
-    
-
-#     results_kappa0 = pairs_results['kappa0_err_lo'] # bug
-#     results_radius = pairs_results['radius_map']
-#     results_chi2_LRT =  pairs_results['chi2_LRT']
-
-#     pl.subplot(2,2,1)
-#     pl.scatter(results_kappa0, results_radius)
-#     pl.xlabel('kappa0 max likelihood')
-#     pl.ylabel('radius max likelihood')
-    
-#     pl.subplot(2,2,2)
-#     pl.scatter(results_kappa0,results_chi2_LRT)
-#     pl.xlabel('kappa0 max likelihood')
-#     pl.ylabel('chi2 p-val')
-
-#     pl.show()
-
-#     import pdb; pdb.set_trace()
-
-
-
-
-   
 def fit_single_filament(save_plots=False):
 
     if args.first == -1: 
@@ -300,7 +130,7 @@ def fit_single_filament(save_plots=False):
         fitobj.halo2_conc = halo2_conc
 
         fitobj.parameters[0]['box']['min'] = 0.
-        fitobj.parameters[0]['box']['max'] = 2e2
+        fitobj.parameters[0]['box']['max'] = 1.5e2
         fitobj.parameters[1]['box']['min'] = 0.001
         fitobj.parameters[1]['box']['max'] = 10
         
@@ -308,7 +138,7 @@ def fit_single_filament(save_plots=False):
         # pl.show()
         # fitobj.save_all_models=False
         log.info('running grid search')
-        n_grid=100
+        n_grid=200
         log_post , params, grid_kappa0, grid_radius = fitobj.run_gridsearch(n_grid=n_grid)
 
         # get the normalised PDF and use the same normalisation on the log
