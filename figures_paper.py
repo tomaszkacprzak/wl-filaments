bug in radius calculation

figures_paper.py
@@ -71,13 +71,23 @@ def figure_density():
 
     param0_old = 0.4
     param1 = 1
+    param1_min = 0.75
+    param1_max = 3.
     param0 = 0.9
+    param0_min = 0.5
+    param0_max = 1.5
     param0_prop = 1.5
 
     list_new = []
     list_old = []
     list_pro = []
     list_m200 = []
+    list_radius = []
+    list_min=[]
+    list_max=[]
+    list_r_min=[]
+    list_r_max=[]
+
 
     for ip,vp in enumerate(pairs[pairs['analysis']==1]):
 
@@ -113,37 +123,79 @@ def figure_density():
         DeltaSigma_at_R200 = (np.abs(h1_DeltaSigma[0])+np.abs(h2_DeltaSigma[0]))/2.
         filament_kappa0 = param0 * DeltaSigma_at_R200 / 1e14
         filament_radius = param1 * (nh1.R_200+nh2.R_200)/2.
+        filament_kappa0_min = param0_min * DeltaSigma_at_R200 / 1e14
+        filament_kappa0_max = param0_max * DeltaSigma_at_R200 / 1e14
+        filament_radius_min = param1_min * (nh1.R_200+nh2.R_200)/2.
+        filament_radius_max = param1_max * (nh1.R_200+nh2.R_200)/2.
 
         DeltaSigma_at_R200_bug = (np.abs(h2g1[0]*h2_Sigma_crit)+np.abs(h1g1[0]*h1_Sigma_crit))/2.
         filament_kappa0_old = param0_old * DeltaSigma_at_R200_bug / 1e14
 
         filament_kappa0_prop = param0_prop * (-1)*(h1g1m[0]+h2g1m[0]) * h2_Sigma_critm / 1e14
 
-        print '% 4d M1=%2.2e M2=%2.2e Mmid=%2.2e DS200=%2.2e kappa0=%2.3f kappa0_bug=%2.3f radius=%2.2f ' % (ip,nh1.M_200,nh2.M_200, (nh1.M_200+nh2.M_200 )/2. ,DeltaSigma_at_R200,filament_kappa0,filament_kappa0_old,filament_radius)
+        radius_grid = np.linspace(-3,3,20000)
+        dr = radius_grid[1]-radius_grid[0]
+        Dtot = np.sqrt(vp['Dxy']**2+vp['Dlos']**2)
+        filament_mass_mid = np.sum(param0     * DeltaSigma_at_R200 / 1e14 * dr / (1+ radius_grid**2/(param1     * (nh1.R_200+nh2.R_200)/2. )**2) )* (Dtot-3) * 1e14
+        filament_mass_min = np.sum(param0_min * DeltaSigma_at_R200 / 1e14 * dr / (1+ radius_grid**2/(param1_min * (nh1.R_200+nh2.R_200)/2. )**2) )* (Dtot-3) * 1e14
+        filament_mass_max = np.sum(param0_max * DeltaSigma_at_R200 / 1e14 * dr / (1+ radius_grid**2/(param1_max * (nh1.R_200+nh2.R_200)/2. )**2) )* (Dtot-3) * 1e14
+
+        print '% 4d M1=%2.2e M2=%2.2e Mmid=%2.2e DS200=%2.2e kappa0=%2.3f kappa0_bug=%2.3f radius=%2.2f Dtot=%2.2f' % (ip,nh1.M_200,nh2.M_200, (nh1.M_200+nh2.M_200 )/2. ,DeltaSigma_at_R200,filament_kappa0,filament_kappa0_old,filament_radius, Dtot)
         print '---- DSmid=%2.2e kappa0=%2.2f' % ((h1_DeltaSigmam[0]+h2_DeltaSigmam[0]),filament_kappa0)
         print '---- %5.3f %5.3e %5.3e %5.3f' % (h1g1[0], h1_DeltaSigma[0], h1_Sigma_crit, h1_kappa[0])
         print '---- %5.3f %5.3e %5.3e %5.3f' % (h2g1[0], h2_DeltaSigma[0], h2_Sigma_crit, h2_kappa[0])
         print '---- change: %2.2f' % (filament_kappa0_old/filament_kappa0)
+        print '---- mass: %2.2e +/- %2.2e %2.2e' % (filament_mass_mid,filament_mass_max,filament_mass_min)
+
+
 
         list_m200.append((nh2.M_200+nh1.M_200)/2.)
         list_old.append(filament_kappa0_old)        
         list_new.append(filament_kappa0)
+        list_min.append(filament_kappa0_min)
+        list_max.append(filament_kappa0_max)
         list_pro.append(filament_kappa0_prop)        
+        list_radius.append(filament_radius)
+        list_r_min.append(filament_radius_min)
+        list_r_max.append(filament_radius_max)
 
     list_m200 = np.array(list_m200)
     list_new = np.array(list_new)
     import fitting
     a,b,Cab = fitting.get_line_fit(list_m200/1e14,list_new,np.ones_like(list_new))
-    print 'line fit a' ,  a
-    print 'line fit b' ,  b
-    print 'line fit C' ,  Cab
+    amin,bmin,Cab = fitting.get_line_fit(list_m200/1e14,list_min,np.ones_like(list_new))
+    amax,bmax,Cab = fitting.get_line_fit(list_m200/1e14,list_max,np.ones_like(list_new))
+    print 'line fit DS a' ,  a
+    print 'line fit DS b' ,  b
+    print 'line fit DS C' ,  Cab
     print 'median Delta Sigma' , np.median(list_new)
 
+    ar,br,Cabr = fitting.get_line_fit(list_m200/1e14,list_radius,np.ones_like(list_new))
+    armin,brmin,Cabr = fitting.get_line_fit(list_m200/1e14,list_r_min,np.ones_like(list_new))
+    armax,brmax,Cabr = fitting.get_line_fit(list_m200/1e14,list_r_max,np.ones_like(list_new))
+    print 'line fit R a' ,  ar
+    print 'line fit R b' ,  br
+    print 'line fit R C' ,  Cabr
+    print 'median Delta Sigma' , np.median(list_new)
+
+
     # pl.plot(list_m200,list_old,'r.')
+    pl.figure()
     pl.plot(list_m200/1e14,list_new,'g.')
     pl.plot(list_m200/1e14,b*list_m200/1e14+a)
+    pl.plot(list_m200/1e14,bmin*list_m200/1e14+amin)
+    pl.plot(list_m200/1e14,bmax*list_m200/1e14+amax)
     pl.xlabel('mean m200')
     pl.ylabel(r'mean \Delta\Sigma')
+    # pl.plot(list_m200,list_pro,'b.')
+
+    pl.figure()
+    pl.plot(list_m200/1e14,list_radius,'g.')
+    pl.plot(list_m200/1e14,br*list_m200/1e14+ar)
+    pl.plot(list_m200/1e14,brmin*list_m200/1e14+armin)
+    pl.plot(list_m200/1e14,brmax*list_m200/1e14+armax)
+    pl.xlabel('mean m200')
+    pl.ylabel(r'mean radius')
     # pl.plot(list_m200,list_pro,'b.')
     pl.show()
 
@@ -600,6 +652,7 @@ def figure_fields():
 
 def figure_random():
 
+    config['use_random_halos'] = True
     import filaments_analyse
     filaments_analyse.config=config
     filaments_analyse.args=args
@@ -637,13 +690,17 @@ def figure_random():
 
     current_id = 0
 
-    dic=tabletools.loadPickle('random.pp2')
-    sum_pdf = dic['sum_pdf']
-    grid_dict = dic['grid_dict']
-    list_prod_2D = dic['list_prod_2D']    
+    use_pickle=False
+
+    if use_pickle:
+        dic=tabletools.loadPickle('random.pp2')
+        sum_pdf = dic['sum_pdf']
+        grid_dict = dic['grid_dict']
+        list_prod_2D = dic['list_prod_2D']    
 
     for ir in range(32):
-        break
+
+        if use_pickle: break
 
         ids = []
 
@@ -654,7 +711,7 @@ def figure_random():
                 ids.append(current_id)
             current_id+=1
 
-        # prod_pdf, grid_dict, list_ids_used , n_pairs_used = filaments_analyse.get_prob_prod_gridsearch_2D(ids , plots=False)
+        prod_pdf, grid_dict, list_ids_used , n_pairs_used = filaments_analyse.get_prob_prod_gridsearch_2D(ids , plots=False)
 
 
         print 'boot % 3d used %d pairs, current_id %d' % (ir,n_pairs_used,current_id)
@@ -671,10 +728,10 @@ def figure_random():
         # pl.show()
 
 
-
-    # sum_pdf = np.zeros_like(prod_pdf)
-    # for lp2D in list_prod_2D: sum_pdf += lp2D
-    # sum_pdf = sum_pdf / np.sum(sum_pdf.flatten())
+    if not use_pickle:
+        sum_pdf = np.zeros_like(prod_pdf)
+        for lp2D in list_prod_2D: sum_pdf += lp2D
+        sum_pdf = sum_pdf / np.sum(sum_pdf.flatten())
 
     contour_levels , contour_sigmas = mathstools.get_sigma_contours_levels(sum_pdf,list_sigmas=[1,2])
 
@@ -685,7 +742,7 @@ def figure_random():
             # xlabel=r' $\Delta\Sigma^{face-on}$ /   $ \mathrm{mean}(\Delta\Sigma_{200})}$ '
             # xlabel=r'$\frac{ \Delta\Sigma_{\mathrm{face-on}}^{\mathrm{fil}} }{ 0.5 (\Delta\Sigma_{200}^{\mathrm{halo1}}+\Delta\Sigma_{200}^{\mathrm{halo2}} )/2}$'
             # ylabel=r'$\frac{ R_{c}^{\mathrm{fil}} }{ (R_{200}^{\mathrm{halo1}}+R_{200}^{\mathrm{halo2}} )/2}$'
-            xlabel=r'$ D^{f} = \Delta\Sigma_{\mathrm{peak}}^{\mathrm{filament}} /  \Delta\Sigma_{200}^{\mathrm{halos}}  $'
+            xlabel=r'$ D^{f} = \Delta\Sigma_{\mathrm{peak}}^{\mathrm{filament}} /  \Delta\Sigma_{R200}^{\mathrm{halos}}  $'
             ylabel=r'$ R^{f} = R_{\mathrm{scale}}^{\mathrm{filament}} /  R_{200}^{\mathrm{halos}} $'
 
     # normal plot
@@ -694,13 +751,13 @@ def figure_random():
     pl.contour(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=contour_levels,colors='y')
      
     # paper plots in ugly colormap
-    pl.figure()
-    cp = pl.contour(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=contour_levels,colors='b')
+    pl.figure(figsize=(8,6))
+    cp = pl.contour(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=contour_levels,colors='b',linewidths=3)
     fmt = {}; strs = [ r'$68\%$', r'$95\%$'] ; 
     # fmt = {}; strs = [ '', '', r'$99\%$'] ; 
     for l,s in zip( cp.levels, strs ): fmt[l] = s
     manual_locations = [(1.1,0.5),(1.5,0.5)]
-    pl.clabel(cp, cp.levels, fmt=fmt , fontsize=12, manual=manual_locations)
+    pl.clabel(cp, cp.levels, fmt=fmt , fontsize=20, manual=manual_locations)
     # pl.pcolormesh(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,cmap=pl.cm.YlOrRd)
     cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=[contour_levels[0],1], alpha=0.2 ,  colors=['b'])
     cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=[contour_levels[1],1], alpha=0.2 ,  colors=['b'])
@@ -709,18 +766,20 @@ def figure_random():
     # cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=[contour_levels[4],1], alpha=0.2 ,  colors=['b'])
     # cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=[contour_levels[0],contour_levels[2]], alpha=0.25 ,  cmap=pl.cm.Blues)
     # cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],sum_pdf,levels=contour_levels, alpha=0.25 ,  cmap=pl.cm.bone)
-    pl.xlabel(xlabel,fontsize=20)
-    pl.ylabel(ylabel,fontsize=20)
+    pl.xlabel(xlabel,fontsize=28)
+    pl.ylabel(ylabel,fontsize=28)
     # pl.title('CFHTLens + BOSS-DR10, using %d halo pairs' % n_pairs_used)
     # pl.plot(max_kappa0,max_radius,'b+',markersize=20,lw=50)
     pl.axis('tight')
     pl.yticks([1,2,3,4])
     # pl.ylim([0,3])
     pl.ylim([0,4])
-    pl.xticks([0.0,0.25,0.5,1.,2])
+    pl.xticks([0.0,0.5,1.,1.5,2])
+    pl.tick_params(axis='both', which='major', labelsize=20)
     # pl.xlim([0,0.8])
-    pl.xlim([0,1])
-    pl.subplots_adjust(bottom=0.12,left=0.1,top=0.95)
+    # pl.xlim([0,1])
+    pl.subplots_adjust(bottom=0.16,left=0.12,top=0.95)
+
 
 
     # pl.figure()
@@ -788,7 +847,7 @@ def figure_contours():
             # xlabel=r' $\Delta\Sigma^{face-on}$ /   $ \mathrm{mean}(\Delta\Sigma_{200})}$ '
             # xlabel=r'$\frac{ \Delta\Sigma_{\mathrm{face-on}}^{\mathrm{fil}} }{ 0.5 (\Delta\Sigma_{200}^{\mathrm{halo1}}+\Delta\Sigma_{200}^{\mathrm{halo2}} )/2}$'
             # ylabel=r'$\frac{ R_{c}^{\mathrm{fil}} }{ (R_{200}^{\mathrm{halo1}}+R_{200}^{\mathrm{halo2}} )/2}$'
-            xlabel=r'$ D^{f} =  \Delta\Sigma_{\mathrm{peak}}^{\mathrm{filament}} /  \Delta\Sigma_{200}^{\mathrm{halos}}  $'
+            xlabel=r'$ D^{f} =  \Delta\Sigma_{\mathrm{peak}}^{\mathrm{filament}} /  \Delta\Sigma_{R200}^{\mathrm{halos}}  $'
             ylabel=r'$ R^{f} =  R_{\mathrm{scale}}^{\mathrm{filament}} /  R_{200}^{\mathrm{halos}} $'
 
     
@@ -818,6 +877,9 @@ def figure_contours():
     # cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],prod_pdf,levels=[contour_levels[4],1], alpha=0.2 ,  colors=['b'])
     # cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],prod_pdf,levels=[contour_levels[0],contour_levels[2]], alpha=0.25 ,  cmap=pl.cm.Blues)
     # cp = pl.contourf(grid_dict['grid_kappa0'],grid_dict['grid_radius'],prod_pdf,levels=contour_levels, alpha=0.25 ,  cmap=pl.cm.bone)
+
+
+
     pl.xlabel(xlabel,fontsize=38)
     pl.ylabel(ylabel,fontsize=38)
     # pl.title('CFHTLens + BOSS-DR10, using %d halo pairs' % n_pairs_used)
@@ -826,11 +888,23 @@ def figure_contours():
     pl.axis('tight')
     pl.yticks([1,2,3,4])
     # pl.ylim([0,3])
-    pl.ylim([0,4])
     pl.xticks([0.0,0.5,1.,1.5,2.0])
     # pl.xlim([0,0.8])
-    pl.xlim([0,2])
+    xmin,xmax = 0,2
+    ymin,ymax = 0,4
+    pl.xlim([xmin,xmax])
+    pl.ylim([ymin,ymax])
     pl.tick_params(axis='both', which='major', labelsize=22)
+
+    # m1 = 0.0878
+    # m2 = 0.0878
+    # ax2 = pl.gca().twiny()
+    # ax2.plot([xmin*m1,xmax*m1],[-1,-1] ,'r')
+    # ax2.set_xticks([0.05,0.1,0.15])
+    # # ax2.xscale('log')
+    # ax2.tick_params(axis='both', which='major', labelsize=22)
+    # ax2.set_xlabel(r'$\Delta\Sigma_{\mathrm{peak}}^{\mathrm{filament}} \ \  \mathrm{M_{\odot} \ Mpc^{-2} h} \ \  \mathrm{for} \ \ \mathrm{M_{200}^{halos}}=10^{14} \ \ \mathrm{M_{\odot}/h}$',fontsize=38)
+
     pl.subplots_adjust(bottom=0.12,left=0.1,top=0.95)
 
     # plot 1d - just kappa0

filaments_analyse.py
@@ -802,6 +802,13 @@ def apply_prior(log_prob,grid_h1M200,grid_h2M200,ids=None):
     prior_dict = tabletools.loadPickle(filename_prior,log=0)
     pairs = tabletools.loadTable(config['filename_pairs'],log=0)
     halos_use = np.concatenate([pairs[ids]['ih1'],pairs[ids]['ih2']])
+
+    if 'use_random_halos' in config:
+        warnings.warn('using random halos')
+        pairs_main = tabletools.loadTable('pairs_cfhtlens_lrgs.fits',log=0)
+        pairs_main = pairs_main[pairs_main['analysis']==1]
+        halos_use = np.concatenate([pairs_main['ih1'],pairs_main['ih2']])
+
     prior_ids = prior_dict['halos_like'][halos_use]
     norm1 = prior_ids-np.kron( np.ones([1,prior_ids.shape[1]]) , prior_ids.max(axis=1)[:,None] )
     norm2 = np.exp(norm1)/np.kron( np.ones([1,prior_ids.shape[1]]) , np.sum(np.exp(norm1),axis=1)[:,None] )
@@ -869,7 +876,6 @@ def get_prob_prod_gridsearch_2D(ids,plots=False,hires=True,hires_marg=False,norm
     n_usable_results=0
 
     filename_grid = '%s/results.grid.%s.pp2' % (args.results_dir,name_data)
-    # filename_grid = 'results_local2scratch/results.grid.%s.pp2' % name_data
     grid_pickle = tabletools.loadPickle(filename_grid)
     grid_kappa0 = grid_pickle['grid_kappa0'][:,:,0,0]
     grid_radius = grid_pickle['grid_radius'][:,:,0,0]
@@ -913,6 +919,7 @@ def get_prob_prod_gridsearch_2D(ids,plots=False,hires=True,hires_marg=False,norm
             except:
                 log_prob = results_pickle
 
+            import pdb; pdb.set_trace()
             # marginal kappa-radius
             # log_prob = results_pickle*214.524/2.577
             warnings.warn('log_prob.shape %s' % str(log_prob.shape))
@@ -1726,6 +1733,7 @@ def plotdata_all():
     # 111 123
 
     remove_list=[317,40,288,426,134,275,60,71,228,21,118,37,146,111] # 011-kappaK rmcloseby
+    # remove_list=[] # 011-kappaK rmcloseby
     # remove_list=[317,283,288,426,145,275,318,176,228,254,236,180,146,123] # 011-kappaK rmcloseby -- rm smaller
     # remove_list=[317,283,288,426,145,275,318,176,228,254,236,180,146,123] # 011-kappaK rmcloseby 
     remove_interloper=[]

filaments_model_2hf.py
@@ -244,55 +244,99 @@ class modelfit():
         halo2_M200 = params[3] * 1e14
 
         self.nh1.M_200= halo1_M200
-        self.nh1.concentr = self.get_concentr(halo1_M200,self.halo1_z)
+        self.nh1.update()
         self.nh1.R_200 = self.nh1.r_s*self.nh1.concentr
 
         self.nh2.M_200= halo2_M200
-        self.nh2.concentr = self.get_concentr(halo2_M200,self.halo2_z)
+        self.nh2.update()
         self.nh2.R_200 = self.nh2.r_s*self.nh2.concentr      
 
         if self.kappa_is_K == 'DS': # model where kappa is dependent on halo mass
+            h1_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
+            h2_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
+            theta1_x=self.nh1.theta_cx+h1_r200_arcmin
+            theta2_x=self.nh1.theta_cx+h1_r200_arcmin
+            h1g1 , h1g2 , h1_DeltaSigma , h1_Sigma_crit, h1_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta1_x,theta1_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            DeltaSigma_at_R200 = (np.abs(h1_DeltaSigma[0])+np.abs(h2_DeltaSigma[0]))/2.
+            filament_kappa0 = params[0]*DeltaSigma_at_R200 / 1e14
+            filament_radius = params[1]*(self.nh1.R_200+self.nh1.R_200)/2.
+            # filament_radius = params[1]
+            if self.n_model_evals % 1000==0:
+                log.info('p[0]=%5.4f p[1]=%5.4f p[2]=%5.4f p[3]=%5.4f kappa0=%5.4e radius=%5.4f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2e m200_2=%5.2e' % ( params[0],params[1],params[2],params[3],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
+
+        elif self.kappa_is_K == 'DSmean': # model where kappa is dependent on halo mass
             h1_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
             h2_r200_arcmin = self.nh2.R_200/cosmology.get_ang_diam_dist(self.halo2_z)/np.pi*180*60
             theta1_x=self.nh1.theta_cx+h1_r200_arcmin
             theta2_x=self.nh2.theta_cx+h2_r200_arcmin
             h1g1 , h1g2 , h1_DeltaSigma , h1_Sigma_crit, h1_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta1_x,theta1_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
-            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh2.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
             DeltaSigma_at_R200 = (np.abs(h1_DeltaSigma[0])+np.abs(h2_DeltaSigma[0]))/2.
             filament_kappa0 = params[0]*DeltaSigma_at_R200 / 1e14
             filament_radius = params[1]*(self.nh1.R_200+self.nh2.R_200)/2.
             # filament_radius = params[1]
             if self.n_model_evals % 1000==0:
-                log.info('p[0]=%5.2f p[1]=%5.2f kappa0=%5.2f radius=%5.2f r200_1=%5.2f r200_2=%5.2f' % ( params[0],params[1],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200 ))
+                log.info('p[0]=%5.4f p[1]=%5.4f p[2]=%5.4f p[3]=%5.4f kappa0=%5.4e radius=%5.4f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2e m200_2=%5.2e' % ( params[0],params[1],params[2],params[3],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
+
+        elif self.kappa_is_K == 'DS-freeR': # model where kappa is dependent on halo mass
+            h1_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
+            h2_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
+            theta1_x=self.nh1.theta_cx+h1_r200_arcmin
+            theta2_x=self.nh1.theta_cx+h1_r200_arcmin
+            h1g1 , h1g2 , h1_DeltaSigma , h1_Sigma_crit, h1_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta1_x,theta1_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            DeltaSigma_at_R200 = (np.abs(h1_DeltaSigma[0])+np.abs(h2_DeltaSigma[0]))/2.
+            filament_kappa0 = params[0]*DeltaSigma_at_R200 / 1e14
+            filament_radius = params[1]
+            # filament_radius = params[1]
+            if self.n_model_evals % 1000==0:
+                log.info('p[0]=%5.4f p[1]=%5.4f p[2]=%5.4f p[3]=%5.4f kappa0=%5.4e radius=%5.4f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2e m200_2=%5.2e' % ( params[0],params[1],params[2],params[3],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
+
+        if self.kappa_is_K == 'DSmean-freeR': # model where kappa is dependent on halo mass
+            h1_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
+            h2_r200_arcmin = self.nh2.R_200/cosmology.get_ang_diam_dist(self.halo2_z)/np.pi*180*60
+            theta1_x=self.nh1.theta_cx+h1_r200_arcmin
+            theta2_x=self.nh2.theta_cx+h2_r200_arcmin
+            h1g1 , h1g2 , h1_DeltaSigma , h1_Sigma_crit, h1_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta1_x,theta1_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh2.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            DeltaSigma_at_R200 = (np.abs(h1_DeltaSigma[0])+np.abs(h2_DeltaSigma[0]))/2.
+            filament_kappa0 = params[0]*DeltaSigma_at_R200 / 1e14
+            filament_radius = params[1]
+            # filament_radius = params[1]
+            if self.n_model_evals % 1000==0:
+                log.info('p[0]=%5.4f p[1]=%5.4f p[2]=%5.4f p[3]=%5.4f kappa0=%5.4e radius=%5.4f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2e m200_2=%5.2e' % ( params[0],params[1],params[2],params[3],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
+
         elif self.kappa_is_K == 'shear' : # model where kappa is dependent on halo mass
             h1_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
             h2_r200_arcmin = self.nh2.R_200/cosmology.get_ang_diam_dist(self.halo2_z)/np.pi*180*60
             theta1_x=self.nh1.theta_cx+h1_r200_arcmin
             theta2_x=self.nh2.theta_cx+h2_r200_arcmin
             h1g1 , h1g2 , h1_DeltaSigma , h1_Sigma_crit, h1_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta1_x,theta1_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
-            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
+            h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh2.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
             DeltaSigma_at_R200 = (np.abs(h1g1[0]*h1_Sigma_crit)+np.abs(h2g1[0]*h2_Sigma_crit))/2.
             filament_kappa0 = params[0]*DeltaSigma_at_R200 / 1e14
             filament_radius = params[1]*(self.nh1.R_200+self.nh2.R_200)/2.
             if self.n_model_evals % 1000==0:
-                log.info('p[0]=%5.2f p[1]=%5.2f kappa0=%5.2f radius=%5.2f r200_1=%5.2f r200_2=%5.2f' % ( params[0],params[1],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200 ))
+                log.info('p[0]=%5.4f p[1]=%5.4f p[2]=%5.4f p[3]=%5.4f kappa0=%5.4e radius=%5.4f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2e m200_2=%5.2e' % ( params[0],params[1],params[2],params[3],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
+
         elif self.kappa_is_K == 'null' : # model where kappa is dependent on halo mass
             h1_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
-            h2_r200_arcmin = self.nh2.R_200/cosmology.get_ang_diam_dist(self.halo2_z)/np.pi*180*60
+            h2_r200_arcmin = self.nh1.R_200/cosmology.get_ang_diam_dist(self.halo1_z)/np.pi*180*60
             theta1_x=self.nh1.theta_cx+h1_r200_arcmin
-            theta2_x=self.nh2.theta_cx+h2_r200_arcmin
+            theta2_x=self.nh1.theta_cx+h1_r200_arcmin
             h1g1 , h1g2 , h1_DeltaSigma , h1_Sigma_crit, h1_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta1_x,theta1_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
             h2g1 , h2g2 , h2_DeltaSigma , h2_Sigma_crit, h2_kappa  = self.nh1.get_shears_with_pz_fast(np.array([theta2_x,theta2_x]) , np.array([0,0]) , self.grid_z_centers , self.prob_z, redshift_offset)
             DeltaSigma_at_R200 = (np.abs(h1g1[0]*h1_Sigma_crit)+np.abs(h2g1[0]*h2_Sigma_crit))/2.
             filament_kappa0 = params[0]*DeltaSigma_at_R200 / 1e14
-            filament_radius = params[1]*(self.nh1.R_200+self.nh2.R_200)/2.
+            filament_radius = params[1]*(self.nh1.R_200+self.nh1.R_200)/2.
             # remove the halos signal completely for random points null test
             self.nh1.M_200= 1e-8
             self.nh2.M_200= 1e-8
             self.nh1.concentr = self.get_concentr(halo1_M200,self.halo1_z)
             self.nh2.concentr = self.get_concentr(halo2_M200,self.halo2_z)
-            if self.n_model_evals % 1000==0:
-                log.info('p[0]=%5.2f p[1]=%5.2f kappa0=%5.2f radius=%5.2f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2f m200_2=%5.2f' % ( params[0],params[1],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
+            if self.n_model_evals % 10==0:
+                log.info('p[0]=%5.4f p[1]=%5.4f p[2]=%5.4f p[3]=%5.4f kappa0=%5.4e radius=%5.4f r200_1=%5.2f r200_2=%5.2f m200_1=%5.2e m200_2=%5.2e' % ( params[0],params[1],params[2],params[3],filament_kappa0,filament_radius,self.nh1.R_200,self.nh2.R_200,self.nh1.M_200,self.nh2.M_200 ))
 
         else:   # standard model
             filament_kappa0 = params[0]