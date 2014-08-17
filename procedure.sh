c994309069f66fc2631a648d0784da1699cfc025
tomaszkacprzak <tomasz.kacprzak.09@ucl.ac.uk>
2014-08-17 22:40:21 +0100

    added procedure

---
+++ procedure.sh
@@ -0,0 +1,27 @@
+# measure BOSS galaxies in CFHTLens
+python ~/code/wl-filaments/halos_cfhtlens.py -c test.yaml -f 0 -n 10 -a select_lrgs
+
+# measure halos and stack
+python ~/code/wl-filaments/halos_cfhtlens.py -c test.yaml -f 0 -n 100 -a fit_halos > run_measure_halos.log
+
+# select filaments
+python ~/code/wl-filaments/filaments_cfhtlens.py -c test.yaml -a get_pairs
+python ~/code/wl-filaments/filaments_cfhtlens.py -c test.yaml -a get_stamps
+
+# make clone - with kappa0=0 
+python ~/code/wl-filaments/filaments_selffit.py -c lrgs.yaml -a get_clone
+
+# make random point
+python ~/code/wl-filaments/halos_cfhtlens.py -c lrgs.yaml -a randomise_halos2
+python ~/code/wl-filaments/filaments_cfhtlens.py -c random.yaml -a get_random_pairs
+python ~/code/wl-filaments/filaments_cfhtlens.py -c random.yaml -a get_stamps
+python ~/code/wl-filaments/filaments_cfhtlens.py -c random.yaml -a add_nfw
+
+# fit all
+python ~/code/wl-filaments/filaments_fit.py -c lrgs.yaml 
+python ~/code/wl-filaments/filaments_fit.py -c random.yaml 
+python ~/code/wl-filaments/filaments_fit.py -c clone.yaml 
+
+
+
+
